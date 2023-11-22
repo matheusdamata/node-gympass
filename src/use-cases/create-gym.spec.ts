9d5487aea60e0cf35588321f25e09233381b687f
Matheus <matheusdamatag@gmail.com>
2023-11-22 02:35:56 -0300

    project started

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CreateGymUseCase } from './create-gym'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'JavaScript GYM',
+      description: null,
+      phone: null,
+      latitude: -16.6926552,
+      longitude: -49.2942842,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
